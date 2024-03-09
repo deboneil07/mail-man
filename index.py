2535465326f550ebf3851e8b08c4b6e5158c227c
Deboneil Bhattacharjee <bhattacharjeedeboneil@gmail.com>
2024-03-09 18:23:49 +0530

    added reset button

--- index.py
+++ index.py
@@ -1,26 +1,25 @@
 from tkinter import *
 from tkinter import ttk, messagebox
+import tkinter as tk
 import webbrowser
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import os
 
-
-
-login_header = ('Arial', 12, 'bold')
 class App:
     def __init__(self, root):
         self.root = root
-        self.root.geometry("400x300")
+        self.root.geometry("400x400")
         self.root.title("Mailman")
+        
         self.login()
 
     def login(self):
         for i in self.root.winfo_children():
             i.destroy()
         self.frame1 = Frame(self.root, width=400, height=400)
-        for i in range(4):
+        for i in range(5):
             self.frame1.rowconfigure(i, minsize=80, weight=1)
         for i in range(3):
             self.frame1.columnconfigure(i, minsize=100, weight=1)
@@ -46,8 +45,18 @@ class App:
 
         self.lbl_help_link = ttk.Label(self.frame1, text="What is App Password?", foreground="blue", cursor="hand2")
         self.lbl_help_link.bind("<Button-1>", self.help_link)
+        self.lbl_help_link.grid(row=3, column=1)   
 
-        self.lbl_help_link.grid(row=3, column=1)
+        self.btn_login = ttk.Button(self.frame1, text="Login")
+        self.btn_login.grid(row=4, column=1, sticky="ew")
+
+        self.btn_reset = ttk.Button(self.frame1, text="Reset", command=self.reset)
+        self.btn_reset.grid(row=4, column=2)
+
+    def reset(self):
+        self.entry_gmail.delete("0", tk.END)
+        self.entry_password.delete("0", tk.END)
+
 
     def help_link(self, event):
         webbrowser.open("https://support.google.com/mail/answer/185833?hl=en#:~:text=Go%20to%20your%20Google%20Account,the%20page%2C%20select%20App%20passwords.", 0)
